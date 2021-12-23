483a33174880e4909df3f8be9304cf97b2a5e1a3
Ivan Zhuro <ivan.zhuro.by@gmail.com>
2021-12-23 17:24:51 +0300

    init: version 1.0

---
+++ src/ts/index.ts
@@ -0,0 +1,7 @@
+// const img = require('./assets/webpack.jpg');
+
+// document.body.innerHTML = `<img src="${img}" alt="webpack.jpg">`;
+
+import '../scss/style.scss';
+
+console.log('Webpack Build');
